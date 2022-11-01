e8a6a50a010b3aa64bfb65fdc6c3faba63fa2311
spiridionefrancescosardina <cicciosardina@live.it>
2022-11-01 19:29:51 +0100

    first commit

--- app.py
+++ app.py
@@ -8,9 +8,4 @@ import plotly.express as px
 # DASHBOARD
 st.title("CLIMATE REVENUE DISCOUNT FACTORS")
 st.write("Factors show the annual increase of company costs measured as percent of industry revenues driven by the carbon price increase. Calculated according to the formula below")
-st.latex(r'''
-    \delta = \frac{\text{Industry GHG Emissions(tn)} \times \text{ETS Price(USD/tn)}}{\text{Industry Output, USD}} \times \text{Tax rate}
-    ''')
-st.write("Tax rate assumed to be 10%, which is the carbon price increase compatible with the 1.5 degree goal")
 
-st.header('Discount Factors per industry and country')
