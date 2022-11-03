b329590834729f713a3fecedc357dca591f0cda5
spiridionefrancescosardina <cicciosardina@live.it>
2022-11-03 21:12:31 +0100

    First str.latex test

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import streamlit as st
-#import pandas as pd
+import pandas as pd
 #import plotly.express as px
 
-#DATA = "data.csv"
+DATA_BALANCESHEET = "balance_sheet.csv"
+
 #ENDER_COLS = ["INDUSTRY CODE DESC", "ANNUAL REVENUE DEDUCTION FACTOR"]
 
 # DASHBOARD
-st.title("CLIMATE REVENUE DISCOUNT FACTORS")
-st.write("Factors show the annual increase of company costs measured as percent of industry revenues driven by the carbon price increase. Calculated according to the formula below")
+st.title("BALANCE SHEET BASED MODEL FOR CREDIT RISK")
+st.write("The model allows to compute the default boundary level based on book value quantities. It is able to link the Credit Risk Literatute with the Financial Statement Analysis.")
 
 
 
+
+st.write("Mathematically speaking, we introduce in the call payoff defining the value of the Equity with the presence of the liquidity shortage, the stochastic liquidity process and the stochastic debt process.")
+st.latex(r'''A_{n}= \f(A_{n-1} \, \c_{n} \)''')
+df_balancesheet = pd.read_csv(DATA_BALANCESHEET)
 #https://cicciosar-dragonfly-app-z8xeob.streamlitapp.com
