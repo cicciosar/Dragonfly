5a87bba7ad6f69391403cc0b6702c7a670db7fb1
spiridionefrancescosardina <cicciosardina@live.it>
2022-11-03 21:19:48 +0100

    First str.latex test bug fixed

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ st.write("The model allows to compute the default boundary level based on book v
 
 st.write("Mathematically speaking, we introduce in the call payoff defining the value of the Equity with the presence of the liquidity shortage, the stochastic liquidity process and the stochastic debt process.")
 st.latex(r'''
-    A_{n}=
-    \f(A_{n-1},c_{n})
+    A_{n}=f(A_{n-1},c_{n})
     ''')
 
 df_balancesheet = pd.read_csv(DATA_BALANCESHEET)
