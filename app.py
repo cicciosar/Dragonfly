9b32e278824b0d76e23412860e0c051f358bc653
spiridionefrancescosardina <cicciosardina@live.it>
2022-11-03 21:18:44 +0100

    First str.latex test

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ st.write("The model allows to compute the default boundary level based on book v
 
 
 st.write("Mathematically speaking, we introduce in the call payoff defining the value of the Equity with the presence of the liquidity shortage, the stochastic liquidity process and the stochastic debt process.")
-st.latex(r'''A_{n}= \f(A_{n-1} \, \c_{n} \)''')
+st.latex(r'''
+    A_{n}=
+    \f(A_{n-1},c_{n})
+    ''')
+
 df_balancesheet = pd.read_csv(DATA_BALANCESHEET)
 #https://cicciosar-dragonfly-app-z8xeob.streamlitapp.com
