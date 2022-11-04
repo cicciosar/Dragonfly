3e316763667647dd7ea5987cd32009ae0082c460
spiridionefrancescosardina <cicciosardina@live.it>
2022-11-04 19:13:12 +0100

    test a latex code

--- app.py
+++ app.py
@@ -21,13 +21,13 @@ st.write("The model allows to compute the default boundary level based on book v
 
 st.write("Mathematically speaking, we introduce in the call payoff defining the value of the Equity with the presence of the liquidity shortage, the stochastic liquidity process and the stochastic debt process.")
 st.latex(r'''
-
 A_{n}=f(A_{n-1},c_{n}),
-
-\\where:
-\\An= vector representing the value of the balance sheet at time n;
-\\cn = vector of financial/economic transactions; 
-\\f (.)= linear affine function in both arguments.
+\begin{tabular}{l @{ $=$ } l}
+$An$ & vector representing the value of the balance sheet at time n;\\
+$cn$ & vector of financial/economic transactions; \\
+$S_d$ & damage level \\
+$f(.)$ & linear affine function in both arguments.
+\end{tabular}   
     ''')
 
 
