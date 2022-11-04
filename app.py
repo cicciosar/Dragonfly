42350d2d3700c444926e4e2451e620a506423af4
spiridionefrancescosardina <cicciosardina@live.it>
2022-11-04 19:05:04 +0100

    test a latex code

--- app.py
+++ app.py
@@ -22,14 +22,12 @@ st.write("The model allows to compute the default boundary level based on book v
 st.write("Mathematically speaking, we introduce in the call payoff defining the value of the Equity with the presence of the liquidity shortage, the stochastic liquidity process and the stochastic debt process.")
 st.latex(r'''
 
-A_{n}=f(A_{n-1},c_{n}
-where:
+A_{n}=f(A_{n-1},c_{n}),
 
-P      notional permeability factor and something longer that needs to be taken at the next line
-N     number of waves 
-S_{d}   damage level
-
-
+\\where:
+\\An= vector representing the value of the balance sheet at time n;
+\\cn = vector of financial/economic transactions; 
+\\f (.)= linear affine function in both arguments.
     ''')
 
 
